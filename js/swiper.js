8b50e6e4e8beeca04facf66f5a554feebce06b04
minnngyu97 <mggkxmjw@gmail.com>
2025-07-28 17:37:34 +0900

    [1.0] 메인페이지 스와이퍼 수정, 푸터 추가

--- js/swiper.js
+++ js/swiper.js
@@ -4,8 +4,17 @@ const slide1 = new Swiper(".slide1", {
     clickable: true,
   },
   autoplay: {
-    delay: 2500,
+    delay: 4000,
     disableOnInteraction: false,
   },
   speed: 1000,
+  on: {
+    slideChangeTransitionEnd: function () {
+      const activeSlide = document.querySelector(".slide1 .swiper-slide-active");
+      const scSlide = document.querySelector(".sc-slide");
+      if (activeSlide && scSlide) {
+        scSlide.style.backgroundColor = activeSlide.dataset.bgColor || "#ffffff";
+      }
+    },
+  },
 });
